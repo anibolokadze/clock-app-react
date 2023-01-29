b77c73380854a2a80846cf93ae52eb795869abee
unknown <anibolokadze@gmail.com>
2023-01-29 09:42:05 +0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <p>Hii</p>
+
+      <header>jbejbf</header>
+
+      <h1>test</h1>
     </div>
   );
 }
