87955d8e1b57b21a4187894ddb5f3c1692845d79
unknown <anibolokadze@gmail.com>
2023-02-07 00:31:53 +0400

    time zone

--- src/Components/CurrentTime/index.tsx
+++ src/Components/CurrentTime/index.tsx
@@ -1,12 +1,60 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function CurrentTime() {
-    fetch('https://geoip-db.com/')
-    .then( res => res.json())
-    .then(response => {
-        console.log("Country: ", response.city);
-     })
-     
-  return <></>;
+  const [time, setTime] = useState(new Date());
+  
+  const localTime = time.toLocaleString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+  const { timeZone } = Intl.DateTimeFormat().resolvedOptions();
+
+  const dayOfTheYear = function getDayOfTheYear(date = new Date()) {
+    const timestamp1 = Date.UTC(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate()
+    );
+    const timestamp2 = Date.UTC(date.getFullYear(), 0, 0);
+    const differenceInMilliseconds = timestamp1 - timestamp2;
+    const differenceInDays = differenceInMilliseconds / 1000 / 60 / 60 / 24;
+    return differenceInDays;
+  };
+
+  useEffect(() => {
+    setInterval(() => setTime(new Date()));
+  }, []);
+
+  return (
+    <>
+      {parseInt(localTime) > 20 || parseInt(localTime) < 6 ? (
+        <>
+          <h2 className="morning">hi baby</h2>
+          <div className="day">
+            <div>{timeZone}</div>
+            <p>{localTime}</p>
+            <p>Day of the year: {dayOfTheYear(new Date())}</p>
+            <p>
+              Day of the week
+              {time.getDay()}
+            </p>
+          </div>
+        </>
+      ) : (
+        <>
+          <h2 className="evening">hi baby</h2>
+          <div className="day">
+            <div>{timeZone}</div>
+            <p>{localTime}</p>
+            <p>Day of the year: {dayOfTheYear(new Date())}</p>
+            <p>
+              Day of the week
+              {time.getDay()}
+            </p>
+          </div>
+        </>
+      )}
+    </>
+  );
 }
