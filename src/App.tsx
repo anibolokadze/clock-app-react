fba3161e05dd3b71f272b80a4d88842ce18ffa95
unknown <anibolokadze@gmail.com>
2023-02-16 17:21:31 +0400

    changes in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
+import { useState, useEffect } from "react";
 import Quotes from "./Components/Quotes";
 import CurrentData from "./Components/CurrentData";
 
 function App() {
+  const [time, setTime] = useState(new Date());
+
+  const onTimeChange = () => {
+    setTime(new Date());
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(onTimeChange, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  let backgroundImage = "bg-image-daytime.jpg";
+  const hour = time.getHours();
+
+  if (hour >= 18 || hour < 5) {
+    backgroundImage = "bg-image-nighttime.jpg";
+  }
+
+  const headerStyle = {
+    backgroundImage: `url("assets/mobile/${backgroundImage}")`,
+    height: "100vh",
+  };
+
   return (
-    <>
+    <div style={headerStyle}>
       <Quotes />
       <CurrentData />
-    </>
+    </div>
   );
 }
 
--- src/Components/CurrentData/index.tsx
+++ src/Components/CurrentData/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import DayImg from "../../assets/mobile/bg-image-daytime.jpg";
+import style from "../../App.module.css";
+
 export default function CurrentData() {
   const [time, setTime] = useState(new Date());
   const [show, setShow] = useState(false);
@@ -8,7 +9,7 @@ export default function CurrentData() {
   const city = { timeZone }.timeZone.split("/").pop();
 
   const localTime = time.toLocaleString("en-US", {
-    hour: "2-digit",
+    hour: "numeric",
     minute: "2-digit",
     hour12: true,
   });
@@ -25,62 +26,50 @@ export default function CurrentData() {
     return differenceInDays;
   };
 
+  const weekNumber = () => {
+    const date = new Date();
+    const time = date.getTime();
+    const yearStart = new Date(date.getFullYear(), 0, 1).getTime();
+    const weekInMilliseconds = 7 * 24 * 60 * 60 * 1000;
+    const result = Math.floor((time - yearStart) / weekInMilliseconds) + 1;
+    return result;
+  };
+
   const handleClick = () => {
     setShow((current) => !current);
   };
 
   useEffect(() => {
-    setInterval(() => setTime(new Date()));
+    setInterval(() => setTime(new Date()), 1000);
   }, []);
+
+  let greeting;
+  if (time.getHours() >= 5 && time.getHours() < 12) {
+    greeting = "GOOD MORNING, IT’S CURRENTLY";
+  } else if (time.getHours() >= 12 && time.getHours() < 18) {
+    greeting = "GOOD AFTERNOON, IT’S CURRENTLY";
+  } else {
+    greeting = "GOOD EVENING, IT’S CURRENTLY";
+  }
 
   return (
     <>
-      <div>
-        {parseInt(localTime) > 20 || parseInt(localTime) < 7 ? 
+      <div className={style.container}>
+        <h2 className="evening">{greeting}</h2>
+        <div className="day">
+          <p>{localTime}</p>
+          <p>In {city}</p>
+        </div>
+        <button onClick={handleClick}>Click</button>
+        {show && (
           <div>
-            <img src={DayImg} />
-            <h2 className="evening">GOOD EVENING, IT’S CURRENTLY</h2>
-            <div className="day">
-              <p>{localTime}</p>
-              <p>In {city}</p>
-            </div>
-
-            <button onClick={handleClick}>Click</button>
-            {show && (
-              <div>
-                <div>{timeZone}</div>
-                <p>Day of the year: {dayOfTheYear(new Date())}</p>
-                <p>
-                  Day of the week
-                  {time.getDay()}
-                </p>
-              </div>
-            )}
+            <div>{timeZone}</div>
+            <p>Day of the year: {dayOfTheYear(new Date())}</p>
+            <p>Day of the week {time.getDay() + 1}</p>
+            <p>Week number: {weekNumber()}</p>
           </div>
-        : 
-          <div>
-            <h2 className="evening">GOOD MORNING, IT’S CURRENTLY</h2>
-            <div className="day">
-              <p>{localTime}</p>
-              <p>In {city}</p>
-            </div>
-
-            <button onClick={handleClick}>Click</button>
-
-            {show && (
-              <div>
-                <div>{timeZone}</div>
-                <p>Day of the year: {dayOfTheYear(new Date())}</p>
-                <p>
-                  Day of the week
-                  {time.getDay()}
-                </p>
-              </div>
-            )}
-          </div>
-        }
+        )}
       </div>
-      <div></div>
     </>
   );
 }
